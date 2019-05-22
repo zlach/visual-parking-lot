67b6d42f03d21de9cc5d2fa3a76f8b6d9af5be36
Zachary Goldberg <zacharysp@gmail.com>
2019-05-22 18:12:33 -0500

    some requirements complete

--- script.js
+++ script.js
@@ -3,11 +3,21 @@ start_button.addEventListener('click', commence);
 var interval_create;
 var interval_move_down;
 var interval_move_up;
+var interval_move_right;
+var interval_move_zone2;
 let bod = document.querySelector('body');
+
+var all_cars = [];
 
 var car_array_down = [];
 var car_array_right = [];
 var car_array_up = [];
+var car_array_zone2 = [];
+
+var qPaused = false;
+
+
+
 
 function commence(){
     start_button.style.display = "none";
@@ -15,28 +25,47 @@ function commence(){
     moveCarsDown();
     moveCarsRight();
     moveCarsUp();
+    moveCarsZone2();
+
 }
 
 function createCars(){
-    interval_create = setInterval(carQ, 1000);
+    interval_create = setInterval(carQ, 1100);
 }
 
 let car_number = 1;
 
+
 function carQ() {
-    var car = document.createElement('img');
-    car.setAttribute("src", randomImage());
-    // car.setAttribute("class", "car");
-    car.style.position = "absolute";
-    car.style.top = "-100px";
-    car.style.left = "100px";
-    car.style.width = "50px";
-    car.style.height = "100px";
-    car.style.transition = "transform .6s";
-    car.setAttribute = ("id", `car${car_number}`);
-    car_array_down.push(car);
-    bod.append(car);
-    car_number += 1;
+    if (car_number == 15){
+        return;
+    } else if (qPaused == true){
+        return;
+    } else {
+        var car = document.createElement('img');
+        car.setAttribute("src", randomImage());
+        // car.setAttribute("class", "car");
+        car.style.position = "absolute";
+        car.style.top = "-100px";
+        car.style.left = "100px";
+        car.style.width = "50px";
+        car.style.height = "100px";
+        car.style.transition = "transform .6s";
+        car.setAttribute("id", `car${car_number}`);
+
+        car.parkTime = randomTime();
+        car.parked = false;
+        car.zone = "";
+
+        car_array_down.push(car);
+        all_cars.push(car);
+        bod.append(car);
+        car_number += 1;
+    }
+}
+
+function randomTime(){
+    return Math.floor((Math.random() * 15000) + 5000);
 }
 
 function randomImage(){
@@ -45,10 +74,16 @@ function randomImage(){
 }
 
 function moveCarsDown() {
+    if (qPaused == true){
+        return;
+    }
     interval_move_down = setInterval(moveDown, 30);
 }
 
 function moveDown() {
+    if (qPaused == true){
+        return;
+    }
     for (let item of car_array_down){
         item.style.top = `${parseInt(item.style.top.replace('px','')) + 4}px`;
     }
@@ -65,11 +100,13 @@ function moveDown() {
 }
 
 function moveCarsRight() {
-    interval_move_down = setInterval(moveRight, 30);
+    interval_move_right = setInterval(moveRight, 30);
 }
 
 function moveRight() {
-    // console.log(car_array_right);
+    if (qPaused == true){
+        return;
+    }
     for (let item of car_array_right){
         item.style.left = `${parseInt(item.style.left.replace('px','')) + 4}px`;
     }
@@ -90,10 +127,144 @@ function moveCarsUp() {
 }
 
 function moveUp() {
+    checkZone1();
     for (let item of car_array_up){
         item.style.top = `${parseInt(item.style.top.replace('px','')) - 4}px`;
+        var position = parseInt(item.style.top.replace('px', ''));
+        if (position > 258){
+            item.zone = "a";
+        } else {
+            item.zone = "b";
+        }
     }
 }    
 
-let zone_one = [{top: 19, occupied: false}, {top: 87, occupied: false}, {top: 155, occupied: false}, {top: 223, occupied: false}, {top: 291, occupied: false}, {top: 359, occupied: false}, {top: 427, occupied: false}, {top: 495, occupied: false}, {top: 563, occupied: false}];
+let zone1 = [{top: 15, open: true}, {top: 82, open: true}, {top: 150, open: true}, {top: 223, open: true}, {top: 291, open: true}, {top: 359, open: true}, {top: 427, open: true}, {top: 495, open: true}, {top: 563, open: true}];
 
+let zone1_parked = 0;
+
+function checkZone1() {
+    if (zone1_parked == 9){
+        parkZone2();
+    } else {
+        for (let i = 0; i < car_array_up.length; i++){
+            var item_top = parseInt(car_array_up[i].style.top.replace('px', ''));
+            for (let spot of zone1){
+                if (item_top < spot.top && spot.open == true){
+                    let car = car_array_up.splice(i, 1)[0];
+                    spot.open = false;
+                    zone1_parked += 1;
+                    parkZone1(car);
+                }
+            }
+        }
+    }    
+}
+
+function parkZone1(car){
+    car.zone = "";
+    car.style.transition = "left .6s linear, transform .3s";
+    car.style.transform = "rotate(-270deg)";
+    car.style.left = "338px";
+    var position = parseInt(car.style.top.replace("px", ""));
+    if (position > 258){
+        setTimeout(function(){leaveZoneA(car)}, car.parkTime);
+    }
+}
+
+// var intervalId_zoneA;
+
+
+
+
+function leaveZoneA(car){
+
+    console.log(car.parkTime, car);
+    car.parked = true;
+    qPaused = true;
+    // zone1_parked -= 1;
+    // intervalId_zoneA = setInterval(() => {checkZoneA(car)}, 100);
+
+    var position_parked = parseInt(car.style.top.replace('px', ''));
+
+    for (let item of all_cars){
+        var position_lane = parseInt(car.style.top.replace('px', ''));
+        console.log(position_lane, position_parked);
+        if (item.zone == 'a' || position_parked > position_lane){
+            setTimeout(() => {leaveZoneA(car)}, 1000);
+            return;
+        }
+    }
+
+    car.zone = "a";
+    car.style.transition = "left .6s linear, transform .6s";
+    car.style.left = "470px";
+    setTimeout(() => {
+        car_array_up.push(car);
+        qPaused = false;
+        car.style.transform = "rotate(-180deg)";
+        car.style.transition = "transform .3s";
+    }, 600);
+    // clearInterval(intervalId_zoneA);
+}
+
+function parkZone2(){
+    for (let i = 0; i < car_array_up.length; i++){
+        var item_top = parseInt(car_array_up[i].style.top.replace('px', ''));
+        if (item_top < 180){
+            let car = car_array_up.splice(i, 1)[0];
+            car.style.transform = "rotate(-90deg)";
+            car_array_zone2.push(car);
+        }
+    }
+}
+
+function moveCarsZone2() {
+    interval_move_zone2 = setInterval(moveZone2, 30);
+}
+
+function moveZone2() {
+    checkZone2();
+    for (let item of car_array_zone2){
+        item.style.left = `${parseInt(item.style.left.replace('px','')) + 4}px`;
+    }
+}
+
+let zone2 = [{top: 270, left: 563, open: true}, {top: 155, left: 633, open: true}, {top: 270, left: 633, open: true}, {top: 155, left: 701, open: true}, {top: 270, left: 701, open: true}, {top: 155, left: 772, open: true}, {top: 270, left: 772, open: true}, {top: 155, left: 841, open: true}, {top: 270, left: 841, open: true}];
+
+let zone2_parked = 0;
+
+function checkZone2(){
+    for (let i = 0; i < car_array_zone2.length; i++){
+        var item_left = parseInt(car_array_zone2[i].style.left.replace('px', ''));
+        for (let spot of zone2){
+            if (item_left > spot.left && spot.open == true){
+                if (spot.top == 270){
+                    let car = car_array_zone2.splice(i, 1)[0];
+                    spot.open = false;
+                    zone2_parked += 1;
+                    parkZone2Right(car);
+                    break;
+                } else if (spot.top == 155){
+                    let car = car_array_zone2.splice(i, 1)[0];
+                    spot.open = false;
+                    zone2_parked += 1;
+                    parkZone2Left(car);
+                    break;
+                }
+            }
+        }
+    }
+}
+
+function parkZone2Right(car){
+    car.style.transition = "top .4s linear, transform .3s";
+    car.style.transform = "rotate(0deg)";
+    car.style.top = "275px";
+}
+
+function parkZone2Left(car){
+    car.style.transition = "top .4s linear, transform .3s";
+    car.style.transform = "rotate(-180deg)";
+    car.style.top = "25px";
+}
